688ae9d4c74e8b1894982dad39d2f4d768696e89
zenkkor <zenkkor@gmail.com>
2016-11-07 17:51:57 +0100

    Add base designs for todo list

--- app/apps/dashboard/controllers/dashboard.js
+++ app/apps/dashboard/controllers/dashboard.js
@@ -5,4 +5,19 @@ app.controller("DashboardCtrl", function($scope) {
 
 	var contract = ToDoList.deployed();
 
+	$scope.todoItems = [
+		{
+			"name":"Wash Laundry",
+			"status": false
+		},
+		{
+			"name":"Wash Dishes",
+			"status": true
+		},
+		{
+			"name":"Take Out Trash",
+			"status": false
+		},
+	];
+
 });
